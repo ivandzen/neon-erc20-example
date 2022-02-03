df73fcf086f814a624bfcd7ca381856f45f02ece
Ivan Loboda <lobodatell@yandex.ru>
2022-02-03 18:50:51 +0300

    Simplify contract, edit readme

--- ERC20Example.sol
+++ ERC20Example.sol
@@ -2,24 +2,6 @@
 
 pragma solidity >=0.5.12;
 
-// ERC20 contract interface definition 
-interface IERC20 {
-    function decimals() external view returns (uint8);
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address who) external view returns (uint256);
-    function allowance(address owner, address spender) external view returns (uint256);
-    function transfer(address to, uint256 value) external returns (bool);
-    function approve(address spender, uint256 value) external returns (bool);
-    function transferFrom(address from, address to, uint256 value) external returns (bool);
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-
-
-    function approveSolana(bytes32 spender, uint64 value) external returns (bool);
-    event ApprovalSolana(address indexed owner, bytes32 indexed spender, uint64 value);
-}
-
 // Default implementation of NEON's ERC20 wrapper contract
 contract NeonERC20Wrapper {
     address constant NeonERC20 = 0xff00000000000000000000000000000000000001;
@@ -28,14 +10,10 @@ contract NeonERC20Wrapper {
     string public symbol;
     bytes32 public tokenMint;
 
-    constructor(
-        string memory _name,
-        string memory _symbol,
-        bytes32 _tokenMint
-    ) {
-        name = _name;
-        symbol = _symbol;
-        tokenMint = _tokenMint;
+    constructor() {
+        name = "TickToken";
+        symbol = "TTOK";
+        tokenMint = 0xf5d945ce24b53ff29297dd1203c7d873dfa846104ca3ec4902507a8f7f6c94c2;
     }
 
     fallback() external {
@@ -47,30 +25,6 @@ contract NeonERC20Wrapper {
         assembly {
             return(add(result, 0x20), mload(result))
         }
-    }
-}
-
-// Testing
-contract ERC20WrapperTest {
-    NeonERC20Wrapper private wrapper;
-
-    constructor() {
-        wrapper = new NeonERC20Wrapper("TestToken", "TTOK", 0x98403300ac23caa27ffa585216f39dcc2ab26b26792506ff4e931e56fc9c31a9);
-    }
-
-    function getWrapperAddress() external view returns (address) {
-        return address(wrapper);
-    }
-
-    function TestTransfer(uint256 amount) external {
-        address from = msg.sender;
-        address to = 0x563d1fbB37385434C85B6564B9A85714C421f0c4;
-        IERC20 erc20wrapped = IERC20(address(wrapper));
-
-        uint256 beginBalance = erc20wrapped.balanceOf(to);
-        require(erc20wrapped.transferFrom(from, to, amount));
-        uint256 endBalance = erc20wrapped.balanceOf(to);
-        require(endBalance - beginBalance == amount);
     }
 }
 
--- README.md
+++ README.md
@@ -46,11 +46,11 @@
 
 8. Convert this Base58 address representation into HEX using [this service](https://appdevtools.com/base58-encoder-decoder) and copy resulting value
 
-9. Replace 3rd argument of NeonERC20Wrapper (line 58) by value got on previous step *NOTE* Add 0x prefix to it
+9. Replace tokenMint value on line 34 by value got on previous step *NOTE* Add 0x prefix to it
   
 10. Load ERC20Example.sol file into [Remix](https://remix.ethereum.org) then compile and deploy it using Injected Web3 Environment on page "Deploy & run transactions" *NOTE* you should be connected to the same Metamask account that was supplied with airdrop on step 5 and to the same network that was setup on step 4.
 
-11. Call getWrappedAddress function and copy resulting ERC20 wrapper contract address
+11. Copy ERC20 contract address
 
 12. Import newly created token into Metamask. Balance should be 0
 
